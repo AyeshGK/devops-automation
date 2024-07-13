85cf41cc35213d317ea743bd2af5ad269a34eb39
Ayesh Vininda <ayeshgk@gmail.com>
2024-07-13 12:11:51 +0530

    initial commit

---
+++ src/test/java/ayeshgk/github/io/devopsautomation/DevopsAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package ayeshgk.github.io.devopsautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
